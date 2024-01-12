c0f75edd70e2c648f794390301d9ac9fb586dbf1
bab29 <brian.bors@cyberark.com>
2024-01-11 16:39:47 -0800

    Updates to fix issues and add functionality

--- Populate_AppLocker.psm1
+++ Populate_AppLocker.psm1
@@ -16,7 +16,7 @@ Function Update-PSMConfigureApplocker {
         $PSMAdminConnect,
         #Location of PSMConfigureAppLocker.xml
         [string]
-        $PSMConfigureAppLocker = ".\PSMConfigureAppLocker.xml",
+        $PSMConfigureAppLocker = "$PWD\PSMConfigureAppLocker.xml",
         <# 
         In order to generate the required CSV run the following PowerShell commands on the PSM after attempting to use the connector
 
@@ -25,7 +25,7 @@ Function Update-PSMConfigureApplocker {
         
         #>
         [string]
-        $ApplockerReportCSV = ".\AppLocker.csv",
+        $ApplockerReportCSV = "$PWD\AppLocker.csv",
         #String Array of files to be added to ignored list
         #(Note: Wildcards not supported at this time)
         # @("File1.exe","File2.dll","File3.exe")
@@ -35,19 +35,27 @@ Function Update-PSMConfigureApplocker {
         #(Note: Wildcards not supported at this time)
         # @("C:\Windows\File1.exe","C:\Windows\system32\File2.dll","C:\Program Files (x86)\Folder\File3.exe")
         [string[]]
-        $ignorePath
+        $ignorePath,
+        #Suppress the output of lines to be added to PSMConfigureAppLocker.xml to the screen
+        [switch]
+        $SuppressOutput,
+        #If it is unable to determine the proper path for %PROGRAMFILES%, do not add the line
+        #Normal behavior is to prompt
+        [switch]
+        $DoNotAddUnknownPaths
     )
+
+    Write-Verbose $("`$PWD - $PWD")
 
     [string[]]$DefaultIgnoreFile = @("CMD.EXE", "CTFMON.EXE", "SETHC.EXE", "TASKFLOWUI.DLL", "ACLAYERS.DLL")
     [string[]]$DefaultIgnorePath = @("%SYSTEM32%\SVCHOST.EXE")
 
     $ignoreFile += $DefaultIgnoreFile
     $ignorePAth += $DefaultIgnorePath
-
+    $ignoreUser += @("NT AUTHORITY\SYSTEM", "NT AUTHORITY\LOCAL SERVICE", "NT AUTHORITY\NETWORK SERVICE")
     Function GetFile([string]$inputString) {
         Return $($inputString.Substring(0, $inputString.IndexOf(" was ")))
     }
-
     Function ConvertPath([string]$inputString) {
         IF ($inputString -like "*%PROGRAMFILES%*" ) {
             If (Test-Path $($inputString.Replace("%PROGRAMFILES%", "C:\Program Files"))) {
@@ -55,38 +63,75 @@ Function Update-PSMConfigureApplocker {
             } elseIf (Test-Path $($inputString.Replace("%PROGRAMFILES%", "C:\Program Files (x86)"))) {
                 $inputString = $inputString.Replace("%PROGRAMFILES%", "C:\Program Files (x86)")
             } else {
-            
+                If ($DoNotAddUnknownPaths) {
+                    return $null
+                }
                 $Choices = @(
-                    [System.Management.Automation.Host.ChoiceDescription]::new("&1 Program Files")
-                    [System.Management.Automation.Host.ChoiceDescription]::new("&2 Program Files(x86)")
+                    [System.Management.Automation.Host.ChoiceDescription]::new("&1 C:\Program Files")
+                    [System.Management.Automation.Host.ChoiceDescription]::new("&2 C:\Program Files (x86)")
+                    [System.Management.Automation.Host.ChoiceDescription]::new("&3 Do Not Add")
+                    [System.Management.Automation.Host.ChoiceDescription]::new("&4 Other Location")
                 )
-                $file = $(Split-Path $inputString -Leaf)
+                $file = $inputString
                 $title = "Unable to locate $file"
                 $question = "Is `"$file`" in `"Program Files`" or `"Program Files `(x86`)`""
-                $decision = $Host.UI.PromptForChoice($title, $question, $choices, 1)
+                $decision = $Host.UI.PromptForChoice($title, $question, $choices, 2)
                 If ($decision -eq 0) {
                     $inputString = $inputString.Replace("%PROGRAMFILES%", "C:\Program Files")
                 } elseif ($decision -eq 1) {
                     $inputString = $inputString.Replace("%PROGRAMFILES%", "C:\Program Files (x86)")
+                } elseif ($decision -eq 3) {
+                    $inputString = $inputString.Replace("%PROGRAMFILES%", "$(Read-Host "Please enter proper location for %PROGRAMFILES%")")
                 }
             }
         }
         $inputString = $inputString.Replace("%SYSTEM32%", "C:\Windows\System32")
+        $inputString = $inputString.Replace("%OSDRIVE%", "C:")
         return [System.Environment]::ExpandEnvironmentVariables("$inputString").toUpper()
     }
+    function Test-Ignore {
+        [CmdletBinding()]
+        param (
+            $Value 
+        )
+    
+        process {
+            If ($(Split-Path $Value.file -Leaf) -in $ignoreFile) {
+                return $true
+            } elseif (($($Value.file) -in $ignorePath)) {
+                Return $true
+            } elseif ($Value.User -in $ignoreUser) {
+                return $true
+            } else {
+                return $false
+            }
+        }
+    }
 
-
-    $CSVInput = Import-Csv $ApplockerReportCSV
-
+    [PSCustomObject]$CSVInput = Import-Csv $ApplockerReportCSV
     $targets = $CSVInput | `
+            Select-Object -Property Message, User -Unique | `
             Select-Object -Property Message, User, @{label = "File"; expression = { $(GetFile($_.Message)).trim() } } -Unique | `
-            Where-Object { $($(Split-Path $_.file -Leaf) -notin $ignoreFile) -and $($($_.file) -notin $ignorePath) } | `
+            #Where-Object { $($(Split-Path $_.file -Leaf) -notin $ignoreFile) -and $($($_.file) -notin $ignorePath) } | `
+            Where-Object { !$(Test-Ignore($PSitem)) } | `
             Sort-Object File 
 
     [pscustomobject]$arrExeApps = @()
     [pscustomobject]$arrElem = @()
-    $targets | ForEach-Object {
 
+    [System.Collections.ArrayList]$targetsFixed = @{}
+    $targets | ForEach-Object {
+        $file = $(ConvertPath($PSItem.File))
+        If (![string]::IsNullOrEmpty($file)) {
+            $targetsFixed += [pscustomobject]@{
+                Message = $PSItem.Message
+                User    = $PSItem.User
+                File    = $file
+            }
+        }
+    }
+
+    $targetsFixed | ForEach-Object {
         $doc = New-Object System.Xml.XmlDocument
         if (".EXE" -eq ([System.IO.Path]::GetExtension("$($PSItem.file)"))) {
             $elem = $doc.CreateElement('Application')
@@ -105,12 +150,12 @@ Function Update-PSMConfigureApplocker {
         $arrExeApps += $doc.InnerXml
         $arrElem += $doc
     }
-    Write-Host "Adding the following items to PSMConfigureAppLocker_Update.xml"
-    $arrExeApps
-
+    If (!$SuppressOutput) {
+        Write-Host "Adding the following items to PSMConfigureAppLocker_Update.xml"
+        $arrExeApps
+    }
     $xmlDoc = New-Object System.Xml.XmlDocument
     $xmlDoc.Load($PSMConfigureAppLocker)
-
     $arrElem.SelectNodes("Application") | ForEach-Object {
         if (![string]::IsNullOrEmpty($PSItem.SessionType)) {
             $node = $xmlDoc.PSMAppLockerConfiguration.InternalApplications
@@ -141,7 +186,7 @@ Function Update-PSMConfigureApplocker {
         }
     }
     $xmldoc.save("$(Split-Path $PSMConfigureAppLocker -Parent)\PSMConfigureAppLocker_Update.xml")
-    Write-Host "Please review PSMConfigureAppLocker_Update.xml prior to import"
+    Write-Host "Please review PSMConfigureAppLocker_Update.xml prior to import" -ForegroundColor Red
 }
 
 Function Export-AppLockerLog {
@@ -193,6 +238,32 @@ Function Set-AppLockerLogsSize {
         $LogSizeMB = 1
     ) 
     $appLockerLog = Get-WinEvent -ListLog "Microsoft-Windows-AppLocker/EXE and DLL"
-    $appLockerLog.MaximumSizeInBytes = $($LogSize*1048576)
+    $appLockerLog.MaximumSizeInBytes = $($LogSizeMB * 1048576)
     $appLockerLog.SaveChanges()
+}
+
+Function Clear-AppLockerLogs {
+    <#
+        .SYNOPSIS
+        Clears Microsoft-Windows-AppLocker/EXE and DLL
+        .DESCRIPTION
+        Clears Microsoft-Windows-AppLocker/EXE and DLL
+    #> 
+    param(
+        #Destination for the log to be saved to
+        [string]$DestinationPath = ".\Microsoft-Windows-AppLocker EXE and DLL $(Get-Date -Format yy-MM-dd-THH-mm )",
+        #Export Event log automatically
+        [switch]$ExportLog
+    )
+    If (!$ExportLog) {
+        $answer = $Host.UI.PromptForChoice('Microsoft-Windows-AppLocker/EXE and DLL', 'Would you like to backup "Microsoft-Windows-AppLocker/EXE and DLL" prior to clearing?', @('&Yes', '&No'), 0)
+        if ($answer -eq 0) {
+            $ExportLog = $true
+        }
+    }
+    If ($ExportLog) {
+        $LogPath = $([System.Environment]::ExpandEnvironmentVariables($(Get-WinEvent -ListLog "Microsoft-Windows-AppLocker/EXE and DLL").LogFilePath))
+        Copy-Item -Path $LogPath -Destination $DestinationPath
+    }
+    wevtutil.exe clear-log "Microsoft-Windows-AppLocker/EXE and DLL"
 }
--- README.md
+++ README.md
@@ -4,25 +4,26 @@
 - Module to automate the configuration of PSMConfigureAppLocker.xml
 
 -----------------
-## Update-PSMConfigureApplocker
+# Update-PSMConfigureApplocker
 
-### SYNOPSIS
+## SYNOPSIS
 Using a csv file containing the events generates a new copy of PSMConfigureAppLocker.xml
 
-### SYNTAX
+## SYNTAX
 
 ```powershell
 Update-PSMConfigureApplocker [-PSMConnect] <String> [-PSMAdminConnect] <String>
  [[-PSMConfigureAppLocker] <String>] [[-ApplockerReportCSV] <String>] [[-ignoreFile] <String[]>]
- [[-ignorePath] <String[]>] [-ProgressAction <ActionPreference>] [<CommonParameters>]
+ [[-ignorePath] <String[]>] [-SuppressOutput] [-DoNotAddUnknownPaths] [-ProgressAction <ActionPreference>]
+ [<CommonParameters>]
 ```
 
-### DESCRIPTION
+## DESCRIPTION
 Using a csv file containing the events generates a new copy of PSMConfigureAppLocker.xml
 
-### PARAMETERS
+## PARAMETERS
 
-#### -PSMConnect
+### -PSMConnect
 Enter the domain and samaccountname of the PSMConnect User
 
 ```yaml
@@ -37,7 +38,7 @@ Accept pipeline input: False
 Accept wildcard characters: False
 ```
 
-#### -PSMAdminConnect
+### -PSMAdminConnect
 Enter the domain and samaccountname of the PSMAdminConnect User
 
 ```yaml
@@ -52,7 +53,7 @@ Accept pipeline input: False
 Accept wildcard characters: False
 ```
 
-#### -PSMConfigureAppLocker
+### -PSMConfigureAppLocker
 Location of PSMConfigureAppLocker.xml
 
 ```yaml
@@ -62,20 +63,17 @@ Aliases:
 
 Required: False
 Position: 3
-Default value: .\PSMConfigureAppLocker.xml
+Default value: "$PWD\PSMConfigureAppLocker.xml"
 Accept pipeline input: False
 Accept wildcard characters: False
 ```
 
-#### -ApplockerReportCSV
+### -ApplockerReportCSV
 In order to generate the required CSV run the following PowerShell commands on the PSM after attempting to use the connector
-
-
 ```powershell
 Function Get-UserFromSID { Param($SID) ($(New-Object System.Security.Principal.SecurityIdentifier($SID)).Translate(\[System.Security.Principal.NTAccount\]))}
 Get-WinEvent -LogName "Microsoft-Windows-AppLocker/EXE and DLL" |Where-Object {$_.LevelDisplayName -ne "Information"} |Select-Object -Property TimeCreated,  @{label="User";expression={Get-UserFromSID($_.UserID)}}, Message |Export-Csv .\AppLocker.csv -NoTypeInformation
 ```
-
 ```yaml
 Type: String
 Parameter Sets: (All)
@@ -83,18 +81,15 @@ Aliases:
 
 Required: False
 Position: 4
-Default value: .\AppLocker.csv
+Default value: "$PWD\AppLocker.csv"
 Accept pipeline input: False
 Accept wildcard characters: False
 ```
 
-#### -ignoreFile
+### -ignoreFile
 String Array of files to be added to ignored list
 (Note: Wildcards not supported at this time)
-```powershell
- -ignoreFile @("File1.exe","File2.dll","File3.exe")
-```
-
+ @("File1.exe","File2.dll","File3.exe")
 
 ```yaml
 Type: String[]
@@ -108,12 +103,10 @@ Accept pipeline input: False
 Accept wildcard characters: False
 ```
 
-#### -ignorePath
+### -ignorePath
 String Array of file paths to be added to ignored list 
 (Note: Wildcards not supported at this time)
-```powershell
--ignorePath @("C:\Windows\File1.exe","C:\Windows\system32\File2.dll","C:\Program Files (x86)\Folder\File3.exe")
-```
+ @("C:\Windows\File1.exe","C:\Windows\system32\File2.dll","C:\Program Files (x86)\Folder\File3.exe")
 
 ```yaml
 Type: String[]
@@ -127,27 +120,71 @@ Accept pipeline input: False
 Accept wildcard characters: False
 ```
 
-#### CommonParameters
-This cmdlet supports the common parameters: -Debug, -ErrorAction, -ErrorVariable, -InformationAction, -InformationVariable, -OutVariable, -OutBuffer, -PipelineVariable, -Verbose, -WarningAction, and -WarningVariable. For more information, see [about_CommonParameters](http://go.microsoft.com/fwlink/?LinkID=113216).
+### -SuppressOutput
+Suppress the output of lines to be added to PSMConfigureAppLocker.xml to the screen
 
------------------
-## Export-AppLockerLog
+```yaml
+Type: SwitchParameter
+Parameter Sets: (All)
+Aliases:
 
-### SYNOPSIS
-Exports AppLocker reports required for Update-PSMConfigureApplocker
-
-### SYNTAX
-
-```powershell
-Export-AppLockerLog [[-ApplockerReportCSV] <String>] [<CommonParameters>]
+Required: False
+Position: Named
+Default value: False
+Accept pipeline input: False
+Accept wildcard characters: False
 ```
 
-### DESCRIPTION
+### -DoNotAddUnknownPaths
+If it is unable to determine the proper path for %PROGRAMFILES%, do not add the line
+Normal behavior is to prompt
+
+```yaml
+Type: SwitchParameter
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: Named
+Default value: False
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -ProgressAction
+{{ Fill ProgressAction Description }}
+
+```yaml
+Type: ActionPreference
+Parameter Sets: (All)
+Aliases: proga
+
+Required: False
+Position: Named
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### CommonParameters
+This cmdlet supports the common parameters: -Debug, -ErrorAction, -ErrorVariable, -InformationAction, -InformationVariable, -OutVariable, -OutBuffer, -PipelineVariable, -Verbose, -WarningAction, and -WarningVariable. For more information, see [about_CommonParameters](http://go.microsoft.com/fwlink/?LinkID=113216).
+# Export-AppLockerLog
+
+## SYNOPSIS
 Exports AppLocker reports required for Update-PSMConfigureApplocker
 
-### PARAMETERS
+## SYNTAX
 
-#### -ApplockerReportCSV
+```powershell
+Export-AppLockerLog [[-ApplockerReportCSV] <String>]
+```
+
+## DESCRIPTION
+Exports AppLocker reports required for Update-PSMConfigureApplocker
+
+## PARAMETERS
+
+### -ApplockerReportCSV
 Filename to use for AppLocker Report
 
 ```yaml
@@ -161,31 +198,23 @@ Default value: .\AppLocker.csv
 Accept pipeline input: False
 Accept wildcard characters: False
 ```
+# Set-AppLockerLogsSize
 
-#### CommonParameters
-This cmdlet supports the common parameters: -Debug, -ErrorAction, -ErrorVariable, -InformationAction, -InformationVariable, -OutVariable, -OutBuffer, -PipelineVariable, -Verbose, -WarningAction, and -WarningVariable. For more information, see [about_CommonParameters](http://go.microsoft.com/fwlink/?LinkID=113216).
-
------------------
-
-## Set-AppLockerLogsSize
-
-### SYNOPSIS
+## SYNOPSIS
 Set maximum log file size for applocker
 
-### SYNTAX
+## SYNTAX
 
-```
-Set-AppLockerLogsSize [[-LogSizeMB] <Int32>] [<CommonParameters>]
+```powershell
+Set-AppLockerLogsSize [[-LogSizeMB] <Int32>]
 ```
 
-### DESCRIPTION
+## DESCRIPTION
 Set maximum log file size for applocker
 
-### EXAMPLES
+## PARAMETERS
 
-### PARAMETERS
-
-#### -LogSizeMB
+### -LogSizeMB
 Size in MB
 
 ```yaml
@@ -199,7 +228,48 @@ Default value: 1
 Accept pipeline input: False
 Accept wildcard characters: False
 ```
+# Clear-AppLockerLogs
 
-#### CommonParameters
-This cmdlet supports the common parameters: -Debug, -ErrorAction, -ErrorVariable, -InformationAction, -InformationVariable, -OutVariable, -OutBuffer, -PipelineVariable, -Verbose, -WarningAction, and -WarningVariable. For more information, see [about_CommonParameters](http://go.microsoft.com/fwlink/?LinkID=113216).
+## SYNOPSIS
+Clears Microsoft-Windows-AppLocker/EXE and DLL
 
+## SYNTAX
+
+```powershell
+Clear-AppLockerLogs [[-DestinationPath] <String>] [-ExportLog]
+```
+
+## DESCRIPTION
+Clears Microsoft-Windows-AppLocker/EXE and DLL
+
+## PARAMETERS
+
+### -DestinationPath
+Destination for the log to be saved to
+
+```yaml
+Type: String
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: 1
+Default value: ".\Microsoft-Windows-AppLocker EXE and DLL $(Get-Date -Format yy-MM-dd-THH-mm )"
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -ExportLog
+Export Event log automatically
+
+```yaml
+Type: SwitchParameter
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: Named
+Default value: False
+Accept pipeline input: False
+Accept wildcard characters: False
+```
